5ffec9d756f7da429089827a16a24f990f2c30cf
Adam Gu <sprinfall@gmail.com>
2018-11-20 15:14:50 +0800

    Refine the Qt example.

--- src/qt_client_async/main.cpp
+++ src/qt_client_async/main.cpp
@@ -5,43 +5,52 @@
 #include <thread>
 
 #define BOOST_ASIO_NO_DEPRECATED
-#include "boost/asio.hpp"
+#include "boost/asio/io_context.hpp"
+#include "boost/asio/executor_work_guard.hpp"
 
 #include <QApplication>
+#include <QDebug>
 
 #include "my_window.h"
 
 int main(int argc, char* argv[]) {
-  try {  // For Asio
+  QApplication app(argc, argv);
 
-    QApplication app(argc, argv);
+  int code = 0;
 
+  // Exception handling is for Asio.
+  try {
     // Run the io_context off in its own thread so that it operates completely
     // asynchronously with respect to the rest of the program.
 
     boost::asio::io_context io_context;
 
+    // Make sure the io_context won't end without work to do.
+    // The variable |work| is not used but necessary for the guard to work.
     auto work = boost::asio::make_work_guard(io_context);
 
-    std::thread asio_thread([&io_context]() { io_context.run(); });
-    // Use bind instead of lambda:
+    // You can also use bind instead of lambda as below:
     //   std::bind(&boost::asio::io_context::run, &io_context)
+    std::thread asio_thread([&io_context]() { io_context.run(); });
 
     // Pass a reference of io_context to MyWindow so that async operations
     // could be created from inside.
     MyWindow my_window(io_context);
     my_window.show();
 
-    int code = app.exec();
+    // The GUI blocks here.
+    code = app.exec();
 
-    // Stop the Asio thread.
+    // The GUI ends, stop the Asio thread.
+    // Pending async operations will be canceled.
     io_context.stop();
+    // Wait for the thread to exit.
     asio_thread.join();
 
-    return code;
-
   } catch (const std::exception& e) {
-    std::cerr << e.what() << std::endl;
-    return 1;
+    qDebug() << "Exception:" << e.what();
+    code = 1;
   }
+
+  return code;
 }
